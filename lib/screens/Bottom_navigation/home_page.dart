07c03c635f985f6c234d18f577f5d44fd04ba2c9
Praveen <69238094+Praveen919@users.noreply.github.com>
2024-12-05 21:54:12 +0530

    Update home_page.dart

--- lib/screens/Bottom_navigation/home_page.dart
+++ lib/screens/Bottom_navigation/home_page.dart
@@ -5,145 +5,163 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Center(
-        child: Column(
-          children: [
-            const SizedBox(height: 10),
-            const Text(
-              "Hello Shakir, Good Day 👋",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              "Search & Land on your dream job",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 30),
-            Stack(
-              alignment: Alignment.topCenter,
-              children: [
-                Container(
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor:  Color(0xFFBFDBFE),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              const Center(
+              child: Text(
+                "Hello Shakir, Good Day 👋",
+                textAlign: TextAlign.center,
+                style: TextStyle(
                   color: Colors.white,
-                  margin: const EdgeInsets.fromLTRB(0, 100, 0, 0),
-                  width: double.maxFinite,
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 90.0, 20, 20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            const Text(
-                              "Recommendation",
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 22,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            GestureDetector(
-                              onTap: () {
-                                // Handle "See all" tap
-                              },
-                              child: const Text(
-                                "See all",
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              ),
+              const SizedBox(height: 10),
+              const Center(
+                child: Text(
+                  "Search & Land on your dream job",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(height: screenHeight * 0.05),
+              Stack(
+                alignment: Alignment.topCenter,
+                children: [
+                  // White container for recommendations
+                  Container(
+                    margin: EdgeInsets.only(top: screenHeight * 0.1),
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(20),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 90, 20, 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                "Recommendation",
                                 style: TextStyle(
-                                  color: Colors.blue,
-                                  fontSize: 18,
+                                  color: Colors.black,
+                                  fontSize: 22,
                                   fontWeight: FontWeight.bold,
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 20),
-                        // List of Recommendations
-                        ListView.builder(
-                          shrinkWrap: true,
-                          physics: const NeverScrollableScrollPhysics(),
-                          itemCount: 5, // Number of items
-                          itemBuilder: (context, index) {
-                            return Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(vertical: 8.0),
-                              child: Card(
-                                elevation: 4,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                                child: ListTile(
-                                  leading: Icon(
-                                    Icons.work_outline,
-                                    color: Colors.blue.shade400,
+                              GestureDetector(
+                                onTap: () {
+                                  // Handle "See all" tap
+                                },
+                                child: const Text(
+                                  "See all",
+                                  style: TextStyle(
+                                    color: Colors.blue,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
                                   ),
-                                  title: Text("Job Title ${index + 1}"),
-                                  subtitle: const Text("Company Name"),
-                                  trailing: const Icon(Icons.arrow_forward),
-                                  onTap: () {
-                                    // Handle card tap
-                                  },
                                 ),
                               ),
-                            );
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Container(
-                  width: 300,
-                  height: 150,
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black26,
-                        blurRadius: 6,
-                        offset: Offset(2, 2),
+                            ],
+                          ),
+                          const SizedBox(height: 20),
+                          // List of Recommendations
+                          ListView.builder(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: 5, // Number of items
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                                child: Card(
+                                  elevation: 4,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: ListTile(
+                                    leading: Icon(
+                                      Icons.work,
+                                      color: Colors.blue.shade400,
+                                    ),
+                                    title: Text("Job Title ${index + 1}"),
+                                    subtitle: const Text("Company Name"),
+                                    trailing: const Icon(Icons.arrow_forward),
+                                    onTap: () {
+                                      // Handle card tap
+                                    },
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
-                  child: const Padding(
-                    padding: EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        TextField(
-                          decoration: InputDecoration(
-                            icon: Icon(Icons.search),
-                            iconColor: Colors.grey,
-                            labelText: "Search Job",
-                            labelStyle: TextStyle(color: Colors.grey),
-                          ),
-                        ),
-                        SizedBox(height: 10),
-                        TextField(
-                          decoration: InputDecoration(
-                            icon: Icon(Icons.room_outlined),
-                            iconColor: Colors.grey,
-                            labelText: "Location",
-                            labelStyle: TextStyle(color: Colors.grey),
-                          ),
+                  // Search Box Container
+                  Container(
+                    width: screenWidth * 0.85,
+                    height: screenHeight * 0.2,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 6,
+                          offset: const Offset(2, 2),
                         ),
                       ],
                     ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const TextField(
+                            decoration: InputDecoration(
+                              icon: Icon(Icons.search),
+                              iconColor: Colors.lightBlueAccent,
+                              labelText: "Search Job",
+                              labelStyle: TextStyle(color: Colors.grey),
+                            ),
+                          ),
+                          const SizedBox(height: 7),
+                          const TextField(
+                            decoration: InputDecoration(
+                              icon: Icon(Icons.room_outlined),
+                              iconColor: Colors.lightBlueAccent,
+                              labelText: "Location",
+                              labelStyle: TextStyle(color: Colors.grey),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
