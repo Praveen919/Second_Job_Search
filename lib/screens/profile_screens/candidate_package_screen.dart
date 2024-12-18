2d5bae8f15f5068e70102930c8031f63e262aa44
x6TTG <kushwahaalok025@gmail.com>
2024-12-18 14:34:22 +0530

    packages screen update

--- lib/screens/profile_screens/candidate_package_screen.dart
+++ lib/screens/profile_screens/candidate_package_screen.dart
@@ -14,24 +14,51 @@ class _SubscriptionScreenState extends State<SubscriptionScreen> {
       'title': 'Basic Plan',
       'description': 'Access to basic features',
       'price': '₹100/monthly',
-      'feature1': '5 paid job posting',
-      'feature2': '2 free job posting',
+      'feature1': '5 paid job applied',
+      'feature2': '2 free job applied',
       'feature3': 'Premium Support 24/7',
     },
     {
       'title': 'Standard Plan',
       'description': 'Access to all standard features',
       'price': '₹500/monthly',
-      'feature1': '20 paid job posting',
-      'feature2': '10 free job posting',
+      'feature1': '20 paid job applied',
+      'feature2': '10 free job applied',
       'feature3': 'Premium Support 24/7',
     },
     {
       'title': 'Premium Plan',
       'description': 'All features included + priority support',
       'price': '₹800/monthly',
-      'feature1': '30 paid job posting',
-      'feature2': '15 free job posting',
+      'feature1': '30 paid job applied',
+      'feature2': '15 free job applied',
+      'feature3': 'Premium Support 24/7',
+    },
+  ];
+
+  final List<Map<String, String>> packages1 = [
+    {
+      'title': 'Basic Plan',
+      'description': 'Access to basic features',
+      'price': '₹100/monthly',
+      'feature1': '5 paid job applied',
+      'feature2': '2 free job applied',
+      'feature3': 'Premium Support 24/7',
+    },
+    {
+      'title': 'Standard Plan',
+      'description': 'Access to all standard features',
+      'price': '₹500/monthly',
+      'feature1': '20 paid job applied',
+      'feature2': '10 free job applied',
+      'feature3': 'Premium Support 24/7',
+    },
+    {
+      'title': 'Premium Plan',
+      'description': 'All features included + priority support',
+      'price': '₹800/monthly',
+      'feature1': '30 paid job applied',
+      'feature2': '15 free job applied',
       'feature3': 'Premium Support 24/7',
     },
   ];
@@ -85,6 +112,117 @@ class _SubscriptionScreenState extends State<SubscriptionScreen> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     const Text(
+                      'Currently active packages',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    ...packages1.map(
+                      (package1) => Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: Container(
+                          padding: const EdgeInsets.all(15),
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 210, 210, 211)
+                                .withOpacity(0.1),
+                            borderRadius: BorderRadius.circular(8),
+                            border: Border.all(color: Colors.blue),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    package1['title']!,
+                                    style: const TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.blue,
+                                    ),
+                                  ),
+                                  Text(
+                                    package1['price']!,
+                                    style: const TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color.fromARGB(255, 0, 0, 0),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 5),
+                              Text(
+                                package1['description']!,
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              const SizedBox(height: 5),
+                              Text(
+                                package1['feature1']!,
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                  color: Color.fromARGB(255, 71, 71, 71),
+                                ),
+                              ),
+                              const SizedBox(height: 5),
+                              Text(
+                                package1['feature2']!,
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                  color: Color.fromARGB(255, 71, 71, 71),
+                                ),
+                              ),
+                              const SizedBox(height: 5),
+                              Text(
+                                package1['feature3']!,
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                  color: Color.fromARGB(255, 71, 71, 71),
+                                ),
+                              ),
+                              const SizedBox(height: 10),
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        _subscribe(package1['title']!);
+                                      },
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: const Color.fromARGB(
+                                            255, 159, 160, 160),
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(8),
+                                        ),
+                                        padding: const EdgeInsets.symmetric(
+                                            vertical: 10),
+                                      ),
+                                      child: const Text(
+                                        'Activatied',
+                                        style: TextStyle(
+                                          fontSize: 14,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
                       'Choose a subscription package',
                       style: TextStyle(
                         fontSize: 18,
@@ -92,112 +230,109 @@ class _SubscriptionScreenState extends State<SubscriptionScreen> {
                       ),
                     ),
                     const SizedBox(height: 15),
-                    ...packages.map((package) => Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 8.0),
-                          child: Container(
-                            padding: const EdgeInsets.all(15),
-                            decoration: BoxDecoration(
-                              color: Colors.blue.withOpacity(0.1),
-                              borderRadius: BorderRadius.circular(8),
-                              border: Border.all(color: Colors.blue),
-                            ),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(
-                                      package['title']!,
-                                      style: const TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.blue,
-                                      ),
-                                    ),
-                                    Text(
-                                      package['price']!,
-                                      style: const TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold,
-                                        color: Color.fromARGB(255, 0, 0, 0),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                const SizedBox(height: 5),
-                                Text(
-                                  package['description']!,
-                                  style: const TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.black,
-                                  ),
-                                ),
-                                const SizedBox(height: 5),
-                                Text(
-                                  package['feature1']!,
-                                  style: const TextStyle(
-                                    fontSize: 14,
-                                    color: Color.fromARGB(255, 71, 71, 71),
-                                  ),
-                                ),
-                                const SizedBox(height: 5),
-                                Text(
-                                  package['feature2']!,
-                                  style: const TextStyle(
-                                    fontSize: 14,
-                                    color: Color.fromARGB(255, 71, 71, 71),
-                                  ),
-                                ),
-                                const SizedBox(height: 5),
-                                Text(
-                                  package['feature3']!,
-                                  style: const TextStyle(
-                                    fontSize: 14,
-                                    color: Color.fromARGB(255, 71, 71, 71),
-                                  ),
-                                ),
-                                const SizedBox(height: 10),
-                                Row(
-                                  children: [
-                                    Expanded(
-                                      child: ElevatedButton(
-                                        onPressed: () {
-                                          _subscribe(package['title']!);
-                                        },
-                                        style: ElevatedButton.styleFrom(
-                                          backgroundColor: Colors.blue,
-                                          shape: RoundedRectangleBorder(
-                                            borderRadius:
-                                                BorderRadius.circular(8),
-                                          ),
-                                          padding: const EdgeInsets.symmetric(
-                                              vertical: 10),
-                                        ),
-                                        child: const Text(
-                                          'Buy Now',
-                                          style: TextStyle(
-                                            fontSize: 14,
-                                            color: Colors.white,
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
+                    ...packages.map(
+                      (package) => Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: Container(
+                          padding: const EdgeInsets.all(15),
+                          decoration: BoxDecoration(
+                            color: Colors.blue.withOpacity(0.1),
+                            borderRadius: BorderRadius.circular(8),
+                            border: Border.all(color: Colors.blue),
                           ),
-                        )),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    package['title']!,
+                                    style: const TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.blue,
+                                    ),
+                                  ),
+                                  Text(
+                                    package['price']!,
+                                    style: const TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color.fromARGB(255, 0, 0, 0),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 5),
+                              Text(
+                                package['description']!,
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              const SizedBox(height: 5),
+                              Text(
+                                package['feature1']!,
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                  color: Color.fromARGB(255, 71, 71, 71),
+                                ),
+                              ),
+                              const SizedBox(height: 5),
+                              Text(
+                                package['feature2']!,
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                  color: Color.fromARGB(255, 71, 71, 71),
+                                ),
+                              ),
+                              const SizedBox(height: 5),
+                              Text(
+                                package['feature3']!,
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                  color: Color.fromARGB(255, 71, 71, 71),
+                                ),
+                              ),
+                              const SizedBox(height: 10),
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        _subscribe(package['title']!);
+                                      },
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: Colors.blue,
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(8),
+                                        ),
+                                        padding: const EdgeInsets.symmetric(
+                                            vertical: 10),
+                                      ),
+                                      child: const Text(
+                                        'Buy Now',
+                                        style: TextStyle(
+                                          fontSize: 14,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ),
-              Container(
-                child: Column(
-                  children: [Text("Currently active packages")],
-                ),
-              )
             ],
           ),
         ),
