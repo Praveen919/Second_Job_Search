413f9e5a3d208f7734bd3917fe8202d85bfcbd92
x6TTG <kushwahaalok025@gmail.com>
2025-01-09 12:00:17 +0530

    updated

--- lib/screens/employeers/company_links_screen.dart
+++ lib/screens/employeers/company_links_screen.dart
@@ -130,7 +130,7 @@ class _CompanyLinksScreenState extends State<CompanyLinksScreen> {
                         Navigator.push(
                           context,
                           MaterialPageRoute(
-                            builder: (context) => const CompanyContactScreen(),
+                            builder: (context) => const CompanyLinksScreen(),
                           ),
                         );
                       },
