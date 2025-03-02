5e5bd172926735a47f5731a3f02ffebe8cb4f97a
Praveen <69238094+Praveen919@users.noreply.github.com>
2025-03-03 00:55:54 +0530

    Update appliedJobRoutes.js

--- backend/routes/appliedJobRoutes.js
+++ backend/routes/appliedJobRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { getAllAppliedJobs, validateIds,getAppliedJobsCount, getAllAppliedJobsWithDetails, updateSeenStatus, 
-       deleteAppliedJob, applyForJob, getApplicationUsingPostId, getApplicationUsingUserId, updateJobStatus } = require('../controllers/appliedJobController');
+const { getAllAppliedJobs, validateIds,getAppliedJobsCount, getAppliedJobsById, getAllAppliedJobsWithDetails, updateSeenStatus, 
+       deleteAppliedJob, applyForJob, getJobsByUserId, getApplicationUsingPostId, getApplicationUsingUserId, getShortlistedCandidates, updateJobStatus } = require('../controllers/appliedJobController');
 const router = express.Router();
 
 router.get('', getAllAppliedJobs);
@@ -9,10 +9,15 @@ router.get('/appliedJobs/details', getAllAppliedJobsWithDetails);
 router.put('/appliedJob/seen', updateSeenStatus);
 router.delete('/appliedJob', deleteAppliedJob);
 router.post('/applyJob', applyForJob);
-router.get('/:user_id',getAppliedJobsCount);
+router.get('/get-job-by-userid/:user_id', getJobsByUserId);
+router.get('/count/:user_id',getAppliedJobsCount);
+router.get('/:user_id', getAppliedJobsById);
 router.get('/user/:user_id', getApplicationUsingUserId);
 router.get('/post/:post_id', getApplicationUsingPostId);
 router.post('/update-job-status/:id', updateJobStatus);
+router.get('/shortlisted/:user_id', getShortlistedCandidates);
+
+
 
 
 module.exports = router;
