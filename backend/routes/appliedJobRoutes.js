55fe24c1d7a0ed0fa286fc8e396a9adfa24745cb
Ajay Kumar <125335090+Ajay7616@users.noreply.github.com>
2025-03-02 11:33:30 +0530

    Update appliedJobRoutes.js

--- backend/routes/appliedJobRoutes.js
+++ backend/routes/appliedJobRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { getAllAppliedJobs, validateIds,getAppliedJobsCount, getAllAppliedJobsWithDetails, updateSeenStatus, deleteAppliedJob, applyForJob } = require('../controllers/appliedJobController');
+const { getAllAppliedJobs, validateIds,getAppliedJobsCount, getAllAppliedJobsWithDetails, updateSeenStatus, 
+       deleteAppliedJob, applyForJob, getApplicationUsingPostId, getApplicationUsingUserId } = require('../controllers/appliedJobController');
 const router = express.Router();
 
 router.get('', getAllAppliedJobs);
@@ -8,6 +9,9 @@ router.get('/appliedJobs/details', getAllAppliedJobsWithDetails);
 router.put('/appliedJob/seen', updateSeenStatus);
 router.delete('/appliedJob', deleteAppliedJob);
 router.post('/applyJob', applyForJob);
-router.get('/:user_id',getAppliedJobsCount)
+router.get('/:user_id',getAppliedJobsCount);
+router.get('/user/:user_id', getApplicationUsingUserId);
+router.get('/post/:post_id', getApplicationUsingPostId);
+
 
 module.exports = router;
