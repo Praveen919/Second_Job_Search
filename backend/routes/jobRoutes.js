227a3d4bd16c16bbcbe936138b4e0b3abbd96213
Praveen <69238094+Praveen919@users.noreply.github.com>
2025-03-27 01:37:30 +0530

    Update jobRoutes.js

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { getAllJobs, postJob, getShortlistedJobsById, getAllJobsCount,getJobCountByUserId, findJobsByUserId } = require('../controllers/jobController');
+const { getAllJobs, postJob, getShortlistedJobsById, getAllJobsCount,getJobCountByUserId,getEmployerJobCounts,updateJob,
+deleteJob,  findJobsByUserId } = require('../controllers/jobController');
 const router = express.Router();
 
 router.get('', getAllJobs);
@@ -8,5 +9,8 @@ router.post('', postJob);
 router.get('/get-shortlisted-job-by-id/:id', getShortlistedJobsById);
 router.get('/find-job/:id', findJobsByUserId);
 router.get('/posted-jobs/:id', getJobCountByUserId);
+router.put('/update/:id', updateJob);
+router.delete('/delete/:id', deleteJob);
+router.get('/counts/:id', getEmployerJobCounts);
 
 module.exports = router;
